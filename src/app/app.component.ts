2e88c30ccdfc3e2fdab56bf5f1fea57a2908cf55
fernandoive <fernandoisaivazquezesquivel@gmail.com>
2021-10-11 17:23:38 -0500

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,45 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // selector: 'lottie-animation-view-demo-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+}) 
+
 export class AppComponent {
   title = 'test';
+
+  public lottieConfig: Object;
+    public anim: any;
+    public animationSpeed: number = 1;
+
+    constructor() {
+        this.lottieConfig = {
+            path: 'assets/location-icon.json',
+            renderer: 'canvas',
+            autoplay: true,
+            loop: true
+        };
+    }
+
+    handleAnimation(anim: any) {
+        this.anim = anim;
+    }
+
+    stop() {
+        this.anim.stop();
+    }
+
+    play() {
+        this.anim.play();
+    }
+
+    pause() {
+        this.anim.pause();
+    }
+
+    setSpeed(speed: number) {
+        this.animationSpeed = speed;
+        this.anim.setSpeed(speed);
+    }
+
 }
